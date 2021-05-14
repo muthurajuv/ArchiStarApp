febc9f0d027a966afe775f237a1ecb8668dbd7b4
muthurajuv <30646551+muthurajuv@users.noreply.github.com>
2021-05-14 15:43:50 +0530

    Add files via upload

---
+++ WelcomeTeam.java
@@ -0,0 +1,7 @@
+public class WelcomeTeam {
+    public static void main(String[] args) {
+        System.out.println("Hello DevOps Team ");
+        System.out.println("Welcomes You All for this new Project");
+	System.out.println("Get ready for the work");
+    }
+}
